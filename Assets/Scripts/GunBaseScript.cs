a829efbfb8dcc5a1972bf7685a2c320f8e6c834f
markver <vereshchaginmark2022@gmail.com>
2024-12-15 17:10:51 +0100

    Cleanup commit
    - Remove unused and depricated files after merge

--- Assets/Scripts/GunBaseScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GunBaseScript : MonoBehaviour
-{ 
-    public float lastShootTime;
-    public int muzzle;
-    public int currentClip;
-    public bool isReloading;
-}
